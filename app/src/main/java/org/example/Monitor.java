b8454c21dea70dbec1ea89d439a02132317e6a31
michael17-maizecorn <mja6332@rit.edu>
2026-09-24 12:04:59 -0400

    Added basic packet receiving for the monitor

--- app/src/main/java/org/example/Monitor.java
+++ app/src/main/java/org/example/Monitor.java
@@ -1,13 +1,36 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Arrays;
+
 /** Entry point for the monitor process. */
 public final class Monitor {
     private Monitor() {}
 
     public static void main(String[] args) {
-        System.out.println("Monitor started.");
-        // TODO: Start monitoring sensor heartbeats here.
+        try(DatagramSocket receiver = new DatagramSocket(4445)){
+            while(true) {
+                byte[] buffer = new byte[4096];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                
+                // Wait for a UDP packet
+                receiver.receive(packet);
+                // Deserialize the packet into a HeartbeatMessage
+                HeartbeatMessage message = HeartbeatMessage.fromByteArray(Arrays.copyOf(packet.getData(), packet.getLength()));
+                
+                // Print the heartbeat message
+                System.out.println("Received heartbeat : message = " + message.toString());
+            }
+            
+            
+            
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
         
-        System.out.println("Monitor ended.");
     }
 }
